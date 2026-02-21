9aae3a5f9aa85b3413a40faf0c897423758dd723
ar4707-design <ar4707@srmist.edu.in>
2026-02-21 23:08:41 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -9,12 +9,18 @@
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("  *****     *****     *****     *****  ");
-        System.out.println(" *     *   *     *   *     *   *       ");
-        System.out.println(" *     *   *     *   *     *   *       ");
-        System.out.println(" *     *   *     *   *****      *****  ");
-        System.out.println(" *     *   *     *   *               * ");
-        System.out.println(" *     *   *     *   *               * ");
-        System.out.println("  *****     *****    *          *****  ");
+        public static void main(String[] args) {
+
+        String line1 = " ***** " + "   " + " ***** " + "   " + " ******" + "   " + " ******";
+        String line2 = "**   **" + "   " + "**   **" + "   " + "**   **" + "   " + "**      ";
+        String line3 = "**   **" + "   " + "**   **" + "   " + "****** " + "   " + " *****  ";
+        String line4 = "**   **" + "   " + "**   **" + "   " + "**      " + "   " + "     **";
+        String line5 = " ***** " + "   " + " ***** " + "   " + "**      " + "   " + " ******";
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
     }
 }
